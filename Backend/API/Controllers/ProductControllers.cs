46068e177c485b3f98f183ff5035a5b63e2cfdd8
taddemillion <tedlamillionyou@gmail.com>
2025-12-08 16:38:30 +0300

    add product controllers to get the products

--- Backend/API/Controllers/ProductControllers.cs
+++ Backend/API/Controllers/ProductControllers.cs
@@ -1,12 +1,28 @@
+using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
+
+[Route("api/products")]
+[ApiController]
 
 public class PoductControllers : ControllerBase
 {
  private  readonly ILogger<PoductControllers> _logger;
- public PoductControllers(ILogger<PoductControllers> logger)
+ private readonly IRepositoryManager _repository;
+ private readonly IMapper _mapper;
+  
+ public PoductControllers(ILogger<PoductControllers> logger, IRepositoryManager repository, IMapper mapper)
  {
     _logger = logger;
+    _repository = repository;
+    _mapper = mapper;
  }
 
+[HttpGet]
+public async Task<IActionResult> GetAllProducts()
+{
+    var products = await _repository.Product.GetAllProductsAsync(trackChanges: false);
+    return Ok(products);
+
+} 
 
 }
